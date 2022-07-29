53ab2a5b3714246c91316aa89ec98b04d3225399
Uttake <kazimirov.anton@mail.ru>
2022-07-29 19:43:16 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = str.split('')
+
+  const checkdeBrackets = array => { 
+    for(let i = 0; i < array.length; i++) {
+      for(let y = 0; y < bracketsConfig.length; y++) {
+        if(array[i] === bracketsConfig[y][0] && array[i + 1] === bracketsConfig[y][1]) {
+          array.splice(i, 2)
+          checkdeBrackets(array)
+        }
+      }
+    }
+  }
+  checkdeBrackets(array)
+  if(array.length !== 0) {
+  return false
+  } else {
+    return true
+  }
 }
